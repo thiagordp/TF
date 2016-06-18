44a6d29b79e753caa8aac875b9b82206a480fa14
Thiago Raulino Dal Pont <thiago.dalpont@outlook.com>
2016-06-18 19:47:05 -0300

    Thiago - Links e arquivo.c

--- arquivo.c
+++ arquivo.c
@@ -3,3 +3,29 @@
 //
 
 #include "arquivo.h"
+
+
+arquivo_t *criaArq(char nome[], char dados[], diretorio_t *diretorio)
+{
+
+}
+
+byte apagaArq(char nome[], diretorio_t *diretorio)
+{
+
+}
+
+arquivo_t *abreArq(char nome[], diretorio_t* diretorio)
+{
+
+}
+
+byte *leArq(char nome[], diretorio_t* diretorio)
+{
+
+}
+
+byte escreveArq(arquivo_t *arquivo, byte *dados)
+{
+
+}
--- diretorio.c
+++ diretorio.c
@@ -2,6 +2,28 @@
 // Created by thiagordp on 13/06/16.
 //
 
+#include<string.h>
 #include "diretorio.h"
 
 
+int criaDir(char nome[], diretorio_t *dirAtual)
+{
+    diretorio_t *novoDiretorio;
+
+    novoDiretorio = (diretorio_t *) malloc(sizeof(diretorio_tr));
+
+    strcpy();
+
+    itemSubDir_t *subDir = dirAtual->listasubDirs;
+    for ()
+}
+
+int apagaDir(char nome[], diretorio_t *dirAtual)
+{
+
+}
+
+int listaSubDir(diretorio_t *dirAtual)
+{
+
+}
