15fed115d8962276244e34c8903a0354542fd71a
Thiago Raulino Dal Pont <thiago.dalpont@outlook.com>
2016-07-09 15:48:43 -0300

    Revert "Continuação do server com pipe e fork"
    This reverts commit 16c9535321625b4b944b07ecfaf98617a61974bb.

--- server_versao2.c
+++ server_versao2.c
@@ -9,29 +9,19 @@
 #include <pthread.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <sys/stat.h>
-#include <ncurses.h>
-#include <fcntl.h>
-#include <unistd.h>
-
 #define CHAVE_MEMORIA_COMPARTILHADA 10
-
 /*
     include das structs no programa cliente, para o mesmo poder ler na memória compartilhada a estrutura de arquivos
 */
 
 
-void *Options(void *);
-
-void *Autenticacao(void *);
-
-void *Verificacao(void *);
+void *Options();
 
 /*
 	semaforo
 */
 sem_t s_cliente;
-sem_t *ptr_semaforo;
+sem_t* ptr_semaforo;
 
 int sock_des_cli;
 
@@ -45,34 +35,30 @@ int main()
     struct sockaddr_in servidor, sock_cli;
 /*
 	variavel mensagem que sera utilizado para escrita no socket
-*/
+*/    
+    char msg_buffer[80];
     int ultimoUser = 0;
 /*
 	inicializa em 1 o semaforo, verifica se inicializou com sucesso
 */
-    if (sem_init(&s_cliente, 1, 1) == -1)
-    {
-        printf("erro ao inicializar o semaforo \n");
-    }
+    if(sem_init(&s_cliente, 1, 1) == -1){
+	printf("erro ao inicializar o semaforo \n");
+  }	
 
 
-/*
+/* 
+
 	 socket() - cria o socket
      AF_INET - domínio do socket (internet).
      SOCK_STREAM - tipo de socket
      0 - protocolo de com. (IP)
-
+     
 */
     if ((socket_des = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         fprintf(stderr, "Erro ao criar o socket do servidor\n");
         exit(0);
     }
-    int x = 1;
-
-    // Sobreescrever quem estiver usando o IP e porta que serão utilizados no código.
-    if (setsockopt(socket_des, SOL_SOCKET, SO_REUSEADDR, &x, sizeof(int)) < 0)
-        printf("setsockopt(SO_REUSEADDR) failed");
 
 /*
  	Atribuição das características do socket.
@@ -87,222 +73,142 @@ int main()
 */
     if (bind(socket_des, (struct sockaddr *) &servidor, sizeof(servidor)) < 0)
     {
-        printf("PROBLEMA: %s\n", strerror(errno));
+	printf("PROBLEMA: %s\n",strerror(errno));
         fprintf(stderr, "Erro de bind\n");
         close(socket_des);  // Fecha
         exit(0);
     }
 
 /*
+
 	Criacao da area de memoria compartilhada
 	shmget = criação de uma área de memória compartilhada
 	shmat = mapeamento de uma área de memória compartilhada
+	
+*/    
 
-*/
-
-    mem_id = shmget(CHAVE_MEMORIA_COMPARTILHADA, sizeof(sem_t), 0777 | IPC_CREAT);
-    if (mem_id < 0)
-    {
-        printf("Erro ao criar area de memoria compartilhada\n");
-        exit(0);
+    mem_id = shmget(CHAVE_MEMORIA_COMPARTILHADA, sizeof(sem_t),0777|IPC_CREAT);
+    if(mem_id < 0){
+    	printf("Erro ao criar area de memoria compartilhada\n");
+    	exit(0);
     }
 
-    ptr_semaforo = (sem_t *) shmat(mem_id, (char *) 0, 0);
-    if (ptr_semaforo == NULL)
-    {
-        printf("Erro de mapeamento de memoria...\n");
-        exit(0);
+    ptr_semaforo = (sem_t*)shmat(mem_id, (char*)0, 0);
+    if(ptr_semaforo == NULL){
+    	printf("Erro de mapeamento de memoria...\n");
+    	exit(0);
     }
     *ptr_semaforo = s_cliente;
-    printf("Endereco semaforo: %p\n", ptr_semaforo);
+    printf("Endereco semaforo: %p\n",ptr_semaforo);	
 
-/*
-
+/* 
+	
 		Habilita o serv. para receber até 10 conexões.
      	socket_des - socket
      	10 - nº máximo de conexões.
+
 */
-    if (listen(socket_des, 10) < 0)
-    {
+    if (listen(socket_des, 10) < 0){
         fprintf(stderr, "Erro de listen\n");
         exit(0);
+    }else{
+        printf("Servidor pronto para conexoes....\n\n");
+        //sem_init(&s_servidor, 0, 1);
     }
 
-    printf("Servidor pronto para conexoes...\n");
-    // sem_init(&s_servidor, 0, 1);
 
-    printf("%d\t", unlink("pipe.servidor"));
-    printf("%d\n", unlink("pipe.autenticador"));
-
-    if (mkfifo("pipe.servidor", 0777) < 0)
+    while(1)
     {
-        printf("Erro ao criar pipe do servidor...\n");
-        exit(0);
-    }
-
-    if (mkfifo("pipe.autenticador", 0777) < 0)
-    {
-        printf("Erro ao criar pipe do cliente...\n");
-        exit(0);
-    }
-
-    printf("pipe criado\n");
-
-    int pid = fork();
-
-    if (pid < 0)
-    {
-        printf("SERVER: erro no fork");
-        exit(0);
-    }
-    else if (pid == 0)
-    {
-        printf("SERVER: filho\n");
-        printf("SERVER: Exec %d\n", execlp("./user_db", "./user_db", NULL));
-        printf("SERVER: depois exec\n");
-    }
-
-    printf("SERVER: iniciando...\n");
-
-    while (1)
-    {
-        printf("A espera de conexões...\n");
-
         fromlen = sizeof(sock_cli); // Tamanho da struct do cliente
 
-        if ((sock_des_cli = accept(socket_des, (struct sockaddr *) &sock_cli,
-                                   (socklen_t *) &fromlen)) < 0)
+        if ((sock_des_cli = accept(socket_des, (struct sockaddr *) &sock_cli, &fromlen)) < 0)
         {
             fprintf(stderr, "Erro de conexao...\n");
             exit(0);
         }
-
-        printf("------------------------------------------\n");
-        printf("conexao aceita\n");
-
-        proc_filho = fork();
-        if (proc_filho == 0)
-        {
-            printf("processo filho id:%d\n", getpid());
-
-            pthread_t new_connection_tid;
-            pthread_create(&new_connection_tid, NULL, Options, NULL);
-            pthread_join(new_connection_tid, NULL);
-
-            close(sock_des_cli);
-            pthread_exit(NULL);
-        }
-        else if (proc_filho > 0)
-        {
-            printf("processo pai: %d\n", getpid());
-        }
         else
         {
-            exit(0);
+		printf("------------------------------------------\n");
+		printf("conexao aceita\n");
+		
+        	proc_filho = fork();
+        	if(proc_filho == 0){
+			printf("processo filho id:%d\n",getpid());
+			printf("------------------------------------------\n");
+        		//Falta declarar as variaveis: operation, option, text 
+        		//read(sock_des_cli,msg_buffer,sizeof(msg_buffer));
+			//printf("depois de ler\n");
+			/*
+        		char *token = NULL;
+
+    			token = strtok(msg_buffer, ";");
+    			operation = token;
+
+    			token = strtok(NULL, ";");
+    			option = token;
+
+    			token = strtok(NULL, ";");
+    			text = token;
+			*/
+			
+			//printf("tenta imprimir\n");
+			//printf("%s\n",msg_buffer);
+			
+			
+			printf("servidor escrevendo no socket\n");			
+			
+			/*
+				Servidor escreve no socket
+			*/
+			int key = 10;
+			write(sock_des_cli, key, sizeof(key));
+			printf("Finalizada a escrita no socket\n");
+			printf("------------------------------------------\n");
+			/*read(sock_des_cli, msg_buffer, sizeof(msg_buffer)+1);
+			printf("Mensagem recebida: %s\n",msg_buffer);*/
+
+				            	
+			pthread_t new_connection_tid;
+	            	//criar a thread para o usuario passa como argumento o proprio
+	            	pthread_create(&new_connection_tid, NULL, Options, NULL);
+			pthread_join(new_connection_tid,NULL);
+			pthread_exit(NULL);
+			close(sock_des_cli);
+        	}else if(proc_filho > 0){
+
+        	}
+
+
+            //retirar usuario de usuarios
+            //usuarios[i] = null;
+        
         }
     }
 }
 
-void *Autenticacao(void *)
+
+void *Options()
 {
-
-    printf("CHEGOU NA THREAD AUTENTICACAO \n");
-
-    char msg_buffer[80];
-    char usuario[30];
-    int fd_server, fd_autenticador, pid;
-
-    // else -- continua a processa.
-
-    /*
-        Abertura do PIPE
-    */
-    printf("Abrindo o pipe do servidor para leitura...\n");
-    fd_server = open("pipe.servidor", O_RDONLY);
-    printf("Abrindo o pipe do autenticador para escrita...\n");
-    fd_autenticador = open("pipe.autenticador", O_WRONLY);
-
-    if (fd_server < 0)
-    {
-        printf("Erro ao abrir PIPE servidor!\n");
-        exit(0);
-    }
-
-    if (fd_autenticador < 0)
-    {
-        printf("Erro ao abrir PIPE cliente!\n");
-        exit(0);
-    }
-
-    printf("Pipes nomeados criados com sucesso!\n");
-    printf("------------------------------------------\n");
-
-    /*
-        Fazer a leitura do usuario que foi escrito no socket pelo cliente
-    */
-
-    printf("Lendo do socket...\n");
-    read(sock_des_cli, usuario, sizeof(usuario) + 1);
-    printf("Usuario conectado: %s\n", usuario);
-
-    /*
-        Escreve no PIPE o usuario passado pelo cliente
-    */
-    write(fd_autenticador, usuario, sizeof(usuario) + 1);
-    //write(fd_autenticador, "", sizeof("") + 1);
-    bzero(msg_buffer, sizeof(msg_buffer));
-
-    printf("SERVER: Lendo a resposta da autenticação...\n");
-    read(fd_server, msg_buffer, sizeof(msg_buffer));
-    printf("SERVER: Mensagem recebida do autenticador...: |%s|\n", msg_buffer);
-
-    if (strcmp(msg_buffer, "V") == 0)
-    {
-        printf("SERVER: Validado\n");
-    }
-    else
-    {
-        printf("SERVER: User invalido.\n");
-    }
-
-    close(fd_server);
-    close(fd_autenticador);
-
-    pthread_exit(NULL);
-}
-
-void *Options(void *)
-{
-    printf("SERVER: CHEGOU NA THREAD VERIFICACAO \n");
-
-    /*
-        Cria a thread de autenticacao
-    */
-    pthread_t autenticacao_tid;
-    pthread_create(&autenticacao_tid, NULL, Autenticacao, NULL);
-    pthread_join(autenticacao_tid, NULL);
-
-    sem_t *ptr_mem;
+  sem_t* ptr_mem;
     int mem_id;
     printf("Cliente conectou!\n");
-    printf("Endereco semaforo: %p\n", ptr_semaforo);
-    printf("------------------------------------------\n");
-    printf("Antes do sem_wait\n");
-    sem_wait(ptr_semaforo);
-    printf("Depois do sem_wait \n");
-    printf("------------------------------------------\n");
-    /*
-        Acesso a sessao critica
-    */
-    sleep(10);
-    printf("SESSAO CRITICA \n");
+	printf("Endereco semaforo: %p\n",ptr_semaforo);
+	printf("------------------------------------------\n");
+	printf("Antes do sem_wait\n");
+	sem_wait(ptr_semaforo);
+	printf("Depois do sem_wait \n");
+	printf("------------------------------------------\n");
+	/*
+		Acesso a sessao critica
+	*/
+	sleep(10);
+	printf("SESSAO CRITICA \n");
 
-    printf("------------------------------------------\n");
-    printf("Antes do sem_post \n");
-    sem_post(ptr_semaforo);
-    printf("Depois do sem_post \n");
-    printf("------------------------------------------\n");
+	printf("------------------------------------------\n");
+	printf("Antes do sem_post \n");		
+	sem_post(ptr_semaforo);	
+	printf("Depois do sem_post \n");
+	printf("------------------------------------------\n");
+	while(1);
 
-    while (1)
-    { }
 }
